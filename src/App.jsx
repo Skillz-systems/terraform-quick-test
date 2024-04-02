3f6c6d60853881e2f2dd26017fa7b6b28c286b64
michael <mike98989@gmail.com>
2024-04-02 10:03:13 +0100

    updated UI again8

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React + Skillz</h1>
+      <h1>Vite + React + Skillz and CircleCI</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
