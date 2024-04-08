c8602a16853ff69ebb679954bc0b1f5d1df49f1d
michael <mike98989@gmail.com>
2024-04-08 20:34:54 +0100

    Commit 2

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React + Skillz and CircleCI & mango</h1>
+      <h1>Vite + React + Skillz and CircleCI & mango+orange+banana</h1>
       <h5>Welcome to home</h5>
       <div className="card">
         <button onClick={() => setCount((count) => count + 2)}>
